Fix the gaussian radius calculation error

src/lib/utils/image.py
@@ -99,19 +99,19 @@ def gaussian_radius(det_size, min_overlap=0.7):
   b1  = (height + width)
   c1  = width * height * (1 - min_overlap) / (1 + min_overlap)
   sq1 = np.sqrt(b1 ** 2 - 4 * a1 * c1)
-  r1  = (b1 + sq1) / 2
+  r1  = (b1 - sq1) / (2 * a1)
 
   a2  = 4
   b2  = 2 * (height + width)
   c2  = (1 - min_overlap) * width * height
   sq2 = np.sqrt(b2 ** 2 - 4 * a2 * c2)
-  r2  = (b2 + sq2) / 2
+  r2  = (b2 - sq2) / (2 * a2)
 
   a3  = 4 * min_overlap
   b3  = -2 * min_overlap * (height + width)
   c3  = (min_overlap - 1) * width * height
   sq3 = np.sqrt(b3 ** 2 - 4 * a3 * c3)
-  r3  = (b3 + sq3) / 2
+  r3  = (b3 + sq3) / (2 * a3)
   return min(r1, r2, r3)
 
 
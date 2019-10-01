0dea414d02fbcefabd8dfc5bce95270314a0b733
Vineeth S. Bhaskara <bhaskaravineeth@gmail.com>
2019-10-01 14:46:28 -0400

    Update command to match the version in the paper for reproducibility

--- experiments/ctdet_coco_hg.sh
+++ experiments/ctdet_coco_hg.sh
@@ -1,6 +1,6 @@
 cd src
 # train
-python main.py ctdet --exp_id coco_hg --arch hourglass --batch_size 24 --master_batch 4 --lr 2.5e-4 --load_model ../models/ExtremeNet_500000.pth --gpus 0,1,2,3,4
+python main.py ctdet --exp_id coco_hg --arch hourglass --batch_size 24 --master_batch 4 --lr 2.5e-4 --load_model ../models/ExtremeNet_250000.pth --gpus 0,1,2,3,4
 # test
 python test.py ctdet --exp_id coco_hg --arch hourglass --keep_res --resume
 # flip test
